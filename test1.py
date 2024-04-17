b5a7d358092a44dca8d248c00423a68850943f5b
bkapturowski <kaptur2601@gmail.com>
2024-04-17 20:18:10 +0200

    First

--- test1.py
+++ test1.py
@@ -2,6 +2,8 @@ from tkinter import *
 from datetime import date
 from tkinter import filedialog
 from tkinter import messagebox
+
+
 from PIL import Image, ImageTk
 import os
 from tkinter.ttk import Combobox
@@ -40,6 +42,105 @@ else:
 
     file.save('Student_data')
 
+
+def Exit():
+    root.destroy()
+###############################ShowImage###########################
+def showimage():
+
+    global filename
+    global img
+
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                          title="Select image file", filetype=(("JPG FIle","*.jpg"),
+                                                                               ("PNG File","*.png"),
+                                                                               ("All files","*.txt")))
+    img = (Image.open(filename))
+    resized_image = img.resize((190,190))
+    photo2 = ImageTk.PhotoImage(resized_image)
+    lbl.config(image=photo2)
+    lbl.image = photo2
+
+#######################Regustration NO.########################
+#now each time we have to enter registrartion number
+
+def registration_no():
+    file = openpyxl.load_workbook('Student_data.xlsx')
+    sheet=file.active
+    row=sheet.max_row
+    max_row_value = sheet.cell(row=row,column=1).value
+
+    try:
+        Registration.set(max_row_value+1)
+
+    except:
+        Registration.set("1")
+
+########################clear##########################
+
+def Clear():
+
+    global img
+    Name.set('')
+    DOB.set('')
+    Religion.set('')
+    Skill.set('')
+    F_Name.set('')
+    M_Name.set('')
+    Father_Occupation.set('')
+    Mother_Occupation.set('')
+    Class.set("Select Class")
+
+
+    registration_no()
+    saveButton.config(state = 'normal')
+
+    img1=PhotoImage(file='Images/upload photo.png')
+    lbl.config(image=img1)
+    lbl.image = img1
+
+    img = ""
+
+def Save():
+    R1 = Registration.get()
+    N1 = Name.get()
+    C1 = Class.get()
+    try:
+        G1=gender
+    except:
+        messagebox.showerror("error","Select Gender!")
+
+    D2 = DOB.get()
+    D1 = Date.get()
+    Re1 = Religion.get()
+    S1 = Skill.get()
+    fathername = F_Name.get()
+    mothername = M_Name.get()
+    F1 = Father_Occupation.get()
+    M1 = Mother_Occupation.get()
+
+    print(R1)
+    print(N1)
+    print(C1)
+    print(G1)
+    print(D2)
+    print(D1)
+    print(Re1)
+    print(S1)
+    print(fathername)
+    print(mothername)
+    print(F1)
+    print(M1)
+
+
+
+def selection():
+    global gender
+    value = radio.get()
+    if value == 1:
+        gender = "Male"
+    else:
+        gender = "Female"
 #top frames
 Label(root, text="Email: kaptur2601@gmail.com", width=10, height=3, bg="#f0687c", anchor='e').pack(side=TOP, fill=X)
 Label(root, text="STUDENT REGISTRATION", width=10, height=2, bg="#f36464",fg='#fff', font='arial 20 bold').pack(side=TOP, fill=X)
@@ -68,7 +169,7 @@ Date = StringVar()
 reg_entry = Entry(root,textvariable=Registration,width=15,font="arial 10")
 reg_entry.place(x=160,y=150)
 
-#registratio_no()
+registration_no()
 
 today = date.today()
 d1 = today.strftime("%d/%m/%Y")
@@ -95,7 +196,33 @@ Name = StringVar()
 name_entry = Entry(obj,textvariable=Name,width=20,font="arial 10")
 name_entry.place(x=160,y=50)
 
+
+DOB = StringVar()
+DOB_entry = Entry(obj,textvariable=DOB,width=20,font="arial 10")
+DOB_entry.place(x=160,y=100)
+
 radio = IntVar()
+R1 = Radiobutton(obj, text="Male", variable=radio, value=1, bg=framebg,fg=framefg,command=selection)
+R1.place(x=150,y=150)
+
+R2 = Radiobutton(obj, text="Female", variable=radio, value=2, bg=framebg,fg=framefg,command=selection)
+R2.place(x=200,y=150)
+
+
+Religion = StringVar()
+Religion_entry = Entry(obj,textvariable=Religion,width=20,font="arial 10")
+Religion_entry.place(x=630,y=100)
+
+Skill = StringVar()
+Skill_entry = Entry(obj,textvariable=Skill,width=20,font="arial 10")
+Skill_entry.place(x=630,y=150)
+
+Class = Combobox(obj, values=['1','2','3','4','5','6','7','8','9','10','11','12'],font="Roboto 10", width=17,state="r")
+Class.place(x=630,y=50)
+Class.set("Select Class")
+
+
+
 
 #Parents details
 
@@ -103,18 +230,53 @@ obj2=LabelFrame(root, text="Parent's Details", font=20,bd=2,width=900,bg=framebg
 obj2.place(x=30,y=470)
 
 
+Label(obj2, text= "Fathers Name:", font="arial 13", bg=framebg, fg = framefg).place(x=30,y=50)
+Label(obj2, text= "Occupation:", font="arial 13", bg=framebg, fg = framefg).place(x=30,y=100)
 
 
+F_Name = StringVar()
+f_entry = Entry(obj2,textvariable=F_Name,width=20,font="arial 10")
+f_entry.place(x=160,y=50)
 
 
+Father_Occupation = StringVar()
+FO_entry = Entry(obj2,textvariable=Father_Occupation,width=20,font="arial 10")
+FO_entry.place(x=160,y=100)
+
+Label(obj2, text= "Mothers Name:", font="arial 13", bg=framebg, fg = framefg).place(x=500,y=50)
+Label(obj2, text= "Occupation:", font="arial 13", bg=framebg, fg = framefg).place(x=500,y=100)
 
 
+M_Name = StringVar()
+m_entry = Entry(obj2,textvariable=M_Name,width=20,font="arial 10")
+m_entry.place(x=630,y=50)
 
 
+Mother_Occupation = StringVar()
+MO_entry = Entry(obj2,textvariable=Mother_Occupation,width=20,font="arial 10")
+MO_entry.place(x=630,y=100)
 
 
+#image
+
+f = Frame(root,bd=3,bg="black",width=200,height=200,relief=GROOVE)
+f.place(x=1000,y=150)
+
+img = PhotoImage(file="Images/upload photo.png")
+lbl = Label(f,bg="black",image=img)
+lbl.place(x=0,y=0)
 
 
+#button
+
+Button(root,text="Upload",width=19,height=2,font="arial 12 bold", bg="lightblue",command=showimage).place(x=1000,y=370)
+
+saveButton = Button(root,text="Save",width=19,height=2,font="arial 12 bold", bg="lightgreen",command=Save)
+saveButton.place(x=1000,y=450)
+
+Button(root,text="Reset",width=19,height=2,font="arial 12 bold", bg="lightpink",command=Clear).place(x=1000,y=530)
+
+Button(root,text="Exit",width=19,height=2,font="arial 12 bold", bg="grey", command=exit).place(x=1000,y=610)
 
 
 
